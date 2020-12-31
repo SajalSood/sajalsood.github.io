73f41c3386757d52526d3b24b50f53436ad49e5e
SajalSood <sajal.sood@gmail.com>
2020-12-31 13:55:21 -0500

    updates

--- serum.exs
+++ serum.exs
@@ -1,6 +1,6 @@
 %{
   site_name: "Sajal Sood",
-  site_description: "TODO: Add site description",
+  site_description: "Coming Soon!",
   date_format: "{WDfull}, {D} {Mshort} {YYYY}",
   base_url: "/",
   author: "John Doe",
