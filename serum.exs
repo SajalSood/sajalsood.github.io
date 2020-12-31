e471489daba1588fe33a5bc46439374722f14008
SajalSood <sajal.sood@gmail.com>
2020-12-31 14:13:12 -0500

    updated details

--- serum.exs
+++ serum.exs
@@ -1,9 +1,9 @@
 %{
   site_name: "Sajal Sood",
-  site_description: "Coming Soon!",
+  site_description: "Coming Soon!!!!!",
   date_format: "{WDfull}, {D} {Mshort} {YYYY}",
   base_url: "/",
-  author: "John Doe",
+  author: "Sajal Sood",
   author_email: "john.doe@example.com",
   plugins: [
     {Serum.Plugins.LiveReloader, only: :dev}
