4c61e35459c167d88b8ff2553a9c59ae6730f31f
SajalSood <sajal.sood@gmail.com>
2020-12-31 22:57:29 -0500

    serum theme blaze published package

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule SajalSood.MixFile do
   defp deps do
     [
       {:serum, "~> 1.5"},
-      {:serum_theme_blaze, git: "https://github.com/SajalSood/serum-theme-blaze"}
+      {:serum_theme_blaze, "~> 1.1"}
       # {:serum_theme_blaze, path: "../serum-theme-blaze"}
     ]
   end
